9bfc95916000e13416c9e38cbc177a5788d9b456
Alexander <127439097+AlexanderMagichi@users.noreply.github.com>
2023-07-29 03:05:34 +0300

    Init Commit

---
+++ Client/src/ua/org/com/alex/clientcode/TestCallLibs.java
@@ -0,0 +1,24 @@
+package ua.org.com.alex.clientcode;
+
+public class TestCallLibs {
+
+	public static void main(String[] args) {
+
+
+		
+		
+		String getTriangle(int size);
+		
+		
+		
+		
+	
+		
+		
+		
+		String shape = shapeBuilder.getTriangle(10);
+		Sys.out.prn(shape);
+
+	}
+
+}
